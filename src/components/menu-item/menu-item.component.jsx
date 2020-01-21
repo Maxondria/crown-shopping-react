2297ba5e38612e74c281c503d9f4b987075f0bdb
Maxon Tayebwa <maxtayebwa@gmail.com>
2020-01-21 20:05:21 +0300

    feat(): break background image into separate div, add styles for hover effect in menu-item component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,10 +3,11 @@ import "./menu-item.styles.scss";
 
 export default ({ title, imageUrl, size }) => {
   return (
-    <div
-      style={{ backgroundImage: `url(${imageUrl})` }}
-      className={`menu-item ${size}`}
-    >
+    <div className={`menu-item ${size}`}>
+      <div
+        className='background-image'
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
       <div className='content'>
         <h1 className='title'>{title.toUpperCase()}</h1>
         <span className='subtitle'>SHOP NOW</span>
