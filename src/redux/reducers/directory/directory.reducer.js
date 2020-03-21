6b9c902b480e4395655ec31a83db22c4cedb78a0
Maxondria <maxtayebwa@gmail.com>
2020-03-21 15:36:51 +0300

    move directory sections to redux store

---
+++ src/redux/reducers/directory/directory.reducer.js
@@ -0,0 +1,4 @@
+import { createReducer } from "../../utils/reducer.utils";
+import { initialState } from "../../store/state/initial.state";
+
+export const directoryReducer = createReducer(initialState.directory, {});
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,14 +1,16 @@
 import { combineReducers } from "redux";
-import { userReducer } from "./user/user.reducer";
-import { cartReducer } from "./cart/cart.reducer";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import { cartReducer } from "./cart/cart.reducer";
+import { directoryReducer } from "./directory/directory.reducer";
+import { userReducer } from "./user/user.reducer";
 
 const persistConfig = { key: "root", storage, whitelist: ["cart"] };
 
 const combinedReducers = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  directory: directoryReducer
 });
 
 export const rootReducer = persistReducer(persistConfig, combinedReducers);
